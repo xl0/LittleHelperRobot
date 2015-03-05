167580f0f49be0a6db16432cc959d0554b3f0042
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2015-03-05 13:13:37 +0100

    Add amazon and facebook support, rework the code a bit

--- littlehelperbot.py
+++ littlehelperbot.py
@@ -11,6 +11,19 @@ reddit = praw.Reddit(user_agent = USER_AGENT)
 reddit.login(REDDIT_USERNAME, REDDIT_PASS)
 comment_stream = praw.helpers.comment_stream(reddit, "all", verbosity=1)
 
+def demobile(expression, substitute, href, text):
+	new_href = re.sub(expression, substitute, href)
+	new_text = re.sub(expression, substitute, text)
+
+	new_href = re.sub("\(", "\\\(", new_href)
+	new_href = re.sub("\)", "\\\)", new_href)
+
+	new_text = re.sub("\[", "\\\[", new_text)
+	new_text = re.sub("\]", "\\\]", new_text)
+
+	return (new_text, new_href)
+
+
 def got_one(comment):
 
 	print "==============="
@@ -31,15 +44,19 @@ def got_one(comment):
 		href = link.get('href')
 		text = link.text
 		if href.find("m.wikipedia.org") != -1:
-			new_href = re.sub("m.wikipedia.org", "wikipedia.org", href)
-			new_text = re.sub("m.wikipedia.org", "wikipedia.org", text)
+			new_text, new_href = demobile("m.wikipedia.org", "wikipedia.org", href, text)
+			links.append((new_text, new_href))
+			print ">>>>> %s %s -> %s %s" % (href.encode('utf-8'), text.encode('utf-8'),
+				new_href.encode('utf-8'), new_text.encode('utf-8'))
 
-			new_href = re.sub("\(", "\\\(", new_href)
-			new_href = re.sub("\)", "\\\)", new_href)
+		if href.find("m.facebook.com") != -1:
+			new_text, new_href = demobile("m.facebook.com", "facebook.com", href, text)
+			links.append((new_text, new_href))
+			print ">>>>> %s %s -> %s %s" % (href.encode('utf-8'), text.encode('utf-8'),
+				new_href.encode('utf-8'), new_text.encode('utf-8'))
 
-			new_text = re.sub("\[", "\\\[", new_text)
-			new_text = re.sub("\]", "\\\]", new_text)
-
+		if href.find('amazon.com/gp/aw/d') != -1:
+			new_text, new_href = demobile("amazon.com/gp/aw/d/", "amazon.com/dp/", href, text)
 			links.append((new_text, new_href))
 			print ">>>>> %s %s -> %s %s" % (href.encode('utf-8'), text.encode('utf-8'),
 				new_href.encode('utf-8'), new_text.encode('utf-8'))
@@ -47,12 +64,13 @@ def got_one(comment):
 	if comment.author.name != "LittleHelperRobot" and len(links) > 0:
 		if len(links) == 1:
 			text = "Non-mobile: [%s](%s)\n\n" % links[0]
-			text += "^I'm ^a ^robot, ^and ^this ^is ^my ^purpose. ^Thank ^you ^for ^all ^the ^kind ^replies! ^PM ^/u/xl0 ^if ^I'm ^causing ^any ^trouble!"
 		else: # len(links) > 1
 			text = "Non-mobile:\n\n"
 			for link in links:
 				text += " * [%s](%s)\n" % (link)
-			text += "\n^I'm ^a ^robot, ^and ^this ^is ^my ^purpose. ^Thank ^you ^for ^all ^the ^kind ^replies! ^PM ^/u/xl0 ^if ^I'm ^causing ^any ^trouble!"
+			text += "\n"
+		text += "^That's ^why ^I'm ^here, ^I ^don't ^judge ^you. ^PM ^/u/xl0 ^if ^I'm ^causing ^any ^trouble. [^WUT?](http://www.reddit.com/r/LittleHelperRobot/comments/2y0irt/whats_this_about/)"
+
 		print "I'm commenting:\n", text.encode('utf-8')
 		try:
 			c = comment.reply(text);
@@ -70,7 +88,7 @@ for comment in comment_stream:
 	if not n % 1000:
 		print n
 
-	if comment.body.lower().find("m.wikipedia.org") != -1:
+	if comment.body.lower().find("m.wikipedia.org") != -1 or \
+		comment.body.lower().find("amazon.com/gp/aw/d") != -1 or \
+		comment.body.lower().find("m.facebook.com") != -1:
 		got_one(comment)
-
-
